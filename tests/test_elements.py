466ecdf106c63dc3485638c818ba3ae0e711441d
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2023-02-18 20:58:20 +0300

    fixed go_to_element in BasePage update test elements added checkboxes test

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,9 +1,10 @@
-from pages.elements_page import TextBoxPage
+import time
+
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
     class TestTextBox:
-
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
@@ -13,3 +14,14 @@ class TestElements:
             assert email == output_email
             assert current_address == output_cur_addr
             assert permanent_address == output_per_addr
+
+    class TestCheckBox:
+        def test_checkbox(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result
+
