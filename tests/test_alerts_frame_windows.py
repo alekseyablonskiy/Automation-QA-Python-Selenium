e1ccc831d7a1f08e8d3b6b5f5af82d7022d59c3c
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2023-02-21 10:38:30 +0300

    added alerts_frame_windows_page.py, alerts_frame_windows_page_locators.py, test_alerts_frame_windows.py

---
+++ tests/test_alerts_frame_windows.py
@@ -0,0 +1,17 @@
+from pages.alerts_frame_windows_page import BrowserWindowsPage
+
+
+class TestAlertsFrameWindows:
+    class TestBrowserWindows:
+        def test_new_tab(self, driver):
+            new_tab_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
+            new_tab_page.open()
+            text_result = new_tab_page.check_opened_tab()
+            assert text_result == 'This is a sample page'
+
+        def test_new_window(self, driver):
+            new_window_page = BrowserWindowsPage(driver, 'https://demoqa.com/browser-windows')
+            new_window_page.open()
+            text_result = new_window_page.check_opened_window()
+            assert text_result == 'This is a sample page'
+
