51560d0b8e50af80b35c8db346135d83c00ef6cb
Aleksey_Yablonskiy <alekseyablonskiy@icloud.com>
2023-02-22 18:35:01 +0300

    updated data.py, generator.py added datepicker tests

--- utils/generator.py
+++ utils/generator.py
@@ -1,9 +1,10 @@
 import random
 
-from data.data import Person, Color
+from data.data import Person, Color, Date
 from faker import Faker
 
 faker_ru = Faker('ru_Ru')
+faker_en = Faker('En')
 Faker.seed()
 
 
@@ -18,11 +19,20 @@ def generated_person():
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
-        mobile=faker_ru.msisdn(),
+        mobile=faker_ru.msisdn()
     )
 
 
 def generated_color():
     yield Color(
         color_name=['Red', 'Blue', 'Green', 'Yellow', 'Purple', 'Black', 'White', 'Voilet', 'Indigo', 'Magenta', 'Aqua']
+    )
+
+
+def generated_date():
+    yield Date(
+        year=faker_en.year(),
+        month=faker_en.month_name(),
+        day=faker_en.day_of_month(),
+        time='12:00'
     )
